286579dd9ddc559ff08b4f8c2075dba31d127845
Ivan <ivanalekseenko.post@gmail.com>
2024-11-16 23:12:18 +0100

    1st commit to the project

--- 1st directory/code/HelloWorld.java
+++ 1st directory/code/HelloWorld.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("You will see 'Hello World!' 8 times: ");
+        for (int i = 0; i < 8; i++) {
+            System.out.println("Hello World!");
+
+        }
+    }
 }
