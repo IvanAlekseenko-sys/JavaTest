bde130c73af778bdd07e5285d6ab69245ab27924
Ivan <ivanalekseenko.post@gmail.com>
2024-11-16 23:17:33 +0100

    2nd commit to the project

--- 1st directory/code/HelloWorld.java
+++ 1st directory/code/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("You will see 'Hello World!' 8 times: ");
-        for (int i = 0; i < 8; i++) {
+        System.out.println("You will see 'Hello World!' 7 times: ");
+        for (int i = 0; i < 7; i++) {
             System.out.println("Hello World!");
 
         }
