5e9fd52b36b68c1a4fb771f379f385d119e0a40b
Ivan <ivanalekseenko.post@gmail.com>
2024-11-21 20:38:57 +0100

    check update

---
+++ Amigo/code/AmigoIsIntelligent.java
@@ -0,0 +1,7 @@
+public class AmigoIsIntelligent {
+    public static void main(String[] args) {
+        System.out.print("Я думаю, быть программистом круто");
+        System.out.println("really?");
+    }
+}
+
